80aaa623dde66f9fdf69edd5c96da4c72c610b93
yamanishiyusuke <nextcentury2000@gmail.com>
2018-09-20 23:04:05 +0900

    add dbmigration

--- mysite/sail/admin.py
+++ mysite/sail/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from sail.models import Book, Impression
 
 # Register your models here.
+
+# admin.site.register(Book)
+# admin.site.register(Impression)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publisher', 'page')
+    list_display_links = ('id', 'name')
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment')
+    list_display_links = ('id', 'comment')
+
+
+admin.site.register(Impression, ImpressionAdmin)
