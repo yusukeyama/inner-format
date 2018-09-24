771244f55c6e8f18f8ddaa51d7a54dd045f2d8c5
yamanishiyusuke <nextcentury2000@gmail.com>
2018-09-24 22:57:14 +0900

    add bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sail'
+    'sail',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
