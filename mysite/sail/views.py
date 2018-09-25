3635c640da0c16be77affcf04a8763ac80b63df6
yamanishiyusuke <nextcentury2000@gmail.com>
2018-09-25 23:19:38 +0900

    add templete

--- mysite/sail/views.py
+++ mysite/sail/views.py
@@ -1,5 +1,22 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from sail.models import Book
 
-# Create your views here.
-def index (request):
-    return HttpResponse("Start")
+
+def book_list(request):
+    """書籍の一覧"""
+    # return HttpResponse('書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render(request,
+                  'sail/book_list.html',    # 使用するテンプレート
+                  {'books': books})         # テンプレートに渡すデータ
+
+
+def book_edit(request, book_id=None):
+    """書籍の編集"""
+    return HttpResponse('書籍の編集')
+
+
+def book_del(request, book_id):
+    """書籍の削除"""
+    return HttpResponse('書籍の削除')
