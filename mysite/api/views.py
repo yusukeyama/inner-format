b9191cee9a4db6fbb381e9cb49ea67562b8d8cfe
yamanishiyusuke <nextcentury2000@gmail.com>
2018-10-01 22:08:37 +0900

    add api view

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,7 +3,42 @@ from collections import OrderedDict
 from django.http import HttpResponse
 from sail.models import Book
 
-# Create your views here.
 
-data = {'id': 1, 'name': 'hoge'}
-json_str = json.dump(data, ensure_ascii=False, indent=2)
+def render_json_response(request, data, status=None):
+    """responseをJSONで返却"""
+    json_str = json.dumps(data, ensure_ascii=False, indent=2)
+    callback = request.GET.get('callback')
+    if not callback:
+        callback = request.POST.get('callback')  # POSTでJSONPの倍医
+    if callback:
+        json_str = "%s(%s)" % (callback, json_str)
+        response = HttpResponse(json_str, content_type='application/javascript; charset=UTF-8', status=status)
+    else:
+        response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=status)
+    return response
+
+
+def book_list(request):
+    """書籍と感想のJSONを返す"""
+    books = []
+    for book in Book.objects.all().order_by('id'):
+
+        impressions = []
+        for impression in book.imporessions.order_by('id'):
+            impression_dict = OrderedDict([
+                ('id', impression.id),
+                ('comment', impression.comment),
+            ])
+            impressions.append(impression_dict)
+
+        book_dict = OrderedDict([
+            ('id', book.id),
+            ('name', book.name),
+            ('publisher', book.publisher),
+            ('page', book.page),
+            ('impressions', impressions)
+        ])
+        books.append(book_dict)
+
+    data = OrderedDict([ ('books', books) ])
+    return render_json_response(request, data)
